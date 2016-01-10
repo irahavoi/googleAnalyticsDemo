efaddd88ea0f70d3f289ff571297b202cf8ea497
Illia Rahavoi <irahavoi@gmail.com>
2016-01-10 17:26:07 -0600

    Added start tracking logic

--- app/src/main/java/com/example/android/dinnerapp/MyApplication.java
+++ app/src/main/java/com/example/android/dinnerapp/MyApplication.java
@@ -2,8 +2,27 @@ package com.example.android.dinnerapp;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Created by irahavoi on 2016-01-10.
  */
 public class MyApplication extends Application{
+    public Tracker mTracker;
+    public void startTracking(){
+        if(mTracker == null){
+            GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+
+            mTracker = ga.newTracker(R.xml.tracker_app); // tracker app is your config xml: tracker_app.xml
+
+            ga.enableAutoActivityReports(this);
+        }
+
+    }
+
+    public Tracker getTracker(){
+        startTracking();
+        return mTracker;
+    }
 }
